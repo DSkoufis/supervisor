2699137c4d11b93f1aa4c74d207e272160dcc19a
DSkoufis <skoufisdimitris@gmail.com>
2018-12-05 00:55:53 +0000

    Starting with on product controller

--- src/main/java/com/supervisor/repository/project/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.supervisor.repository.project;
-
-import com.supervisor.domain.project.Project;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRepository extends CrudRepository<Project, Long> {
-}
