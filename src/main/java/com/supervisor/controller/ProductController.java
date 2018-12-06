bd315fe8a12c29de1ef9d325f73871f39508e00c
DSkoufis <skoufisdimitris@gmail.com>
2018-12-06 23:54:07 +0000

    ProductController refactor (WIP)

--- src/main/java/com/supervisor/controller/ProductController.java
+++ src/main/java/com/supervisor/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.supervisor.controller;
 
 import com.supervisor.dao.ProductDao;
 import com.supervisor.domain.product.Product;
+import com.supervisor.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,11 +16,11 @@ import java.util.List;
 public class ProductController {
 
     @Autowired
-    private ProductDao productDao;
+    private ProductService productService;
 
-    @RequestMapping(value = {"index", "/"})
+    @GetMapping(value = {"index", "/"})
     public String index(Model model) {
-    	List<Product> productList = productDao.findAll();
+    	List<Product> productList = productService.getProducts();
     	model.addAttribute("products", productList);
         return "";
     }
