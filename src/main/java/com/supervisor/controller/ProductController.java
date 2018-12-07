b41106e395e96ae109ffcd309bbe5ec09380e7e2
DSkoufis <skoufisdimitris@gmail.com>
2018-12-07 00:27:16 +0000

    Adding product creation methods

--- src/main/java/com/supervisor/controller/ProductController.java
+++ src/main/java/com/supervisor/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.supervisor.controller;
 
+import com.supervisor.command.ProductCreateCommand;
 import com.supervisor.dao.ProductDao;
 import com.supervisor.domain.product.Product;
 import com.supervisor.service.ProductService;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +25,12 @@ public class ProductController {
     public String index(Model model) {
     	List<Product> productList = productService.getProducts();
     	model.addAttribute("products", productList);
-        return "";
+        return ""; // TODO return the view
+    }
+
+    @PutMapping(value = "create")
+    public String createProduct(@RequestBody ProductCreateCommand cmd) {
+        productService.createProduct(cmd);
+        // TODO return something
     }
 }
