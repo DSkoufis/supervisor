46cfce5e4b0858318b0170e09e0f24509f36fa0b
DSkoufis <skoufisdimitris@gmail.com>
2019-01-16 01:08:14 +0000

    Set up view resolver Now controllers can serve jsp pages. Added configuration to support such functionality.
    All jsp files should be placed under /src/webapp/WEB-INF/views/ directory

--- src/main/java/com/supervisor/controller/ProductController.java
+++ src/main/java/com/supervisor/controller/ProductController.java
@@ -5,12 +5,11 @@ import com.supervisor.domain.product.Product;
 import com.supervisor.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -18,30 +17,26 @@ import java.util.List;
 @RequestMapping(value = "/product")
 public class ProductController {
 
+    private static final String VIEW_PATH_DIR = "product/";
+
     private final ProductService productService;
 
-	@Autowired
-	public ProductController(ProductService productService) {
-		this.productService = productService;
-	}
-
-	@GetMapping(value = {"index", "/"})
-    public RedirectView index() {
-		RedirectView redirectView = new RedirectView();
-		redirectView.setUrl("list");
-        return redirectView;
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
-    @GetMapping(value = "list")
-	public String listProducts(Model model) {
-		List<Product> products = productService.getAllProducts();
-		model.addAttribute("products", products);
-		return "list";
-	}
+    @GetMapping(value = {"", "/"})
+    public ModelAndView index() {
+        ModelAndView model = new ModelAndView(VIEW_PATH_DIR + "productList");
+        List<Product> products = productService.getAllProducts();
+        model.addObject("products", products);
+        return model;
+    }
 
     @PostMapping(value = "create")
     public void createProduct(@RequestBody ProductCreateCommand cmd) {
-		// TODO Could this return the product? think so
+        // TODO Could this return the product? think so
         productService.createProduct(cmd);
     }
 }
