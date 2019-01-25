d6d56b3ed2005ed132febe0bb1b66953b6594096
DSkoufis <skoufisdimitris@gmail.com>
2019-01-25 11:28:15 +0000

    Moved static resources and mapped them to be able to serve them using spring boot

--- src/main/java/com/supervisor/configuration/MvcConfiguration.java
+++ src/main/java/com/supervisor/configuration/MvcConfiguration.java
@@ -3,8 +3,11 @@ package com.supervisor.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
@@ -17,5 +20,23 @@ public class MvcConfiguration implements WebMvcConfigurer {
         bean.setPrefix("/WEB-INF/views/");
         bean.setSuffix(".jsp");
         return bean;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/libraries/**")
+                .addResourceLocations("/resources/libraries/").setCachePeriod(3600)
+                .resourceChain(true).addResolver(new PathResourceResolver());
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("/resources/js/").setCachePeriod(3600)
+                .resourceChain(true).addResolver(new PathResourceResolver());
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/resources/css/").setCachePeriod(3600)
+                .resourceChain(true).addResolver(new PathResourceResolver());
     }
 }
