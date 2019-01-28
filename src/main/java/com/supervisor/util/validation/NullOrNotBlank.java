0d0602d5cddcf76522385428d28f364fd571b311
DSkoufis <skoufisdimitris@gmail.com>
2019-01-28 01:37:34 +0000

    A new constraint that permits null or non empty values. It could be used as a template in the future

---
+++ src/main/java/com/supervisor/util/validation/NullOrNotBlank.java
@@ -0,0 +1,35 @@
+package com.supervisor.util.validation;
+
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@ConstraintComposition(CompositionType.OR)
+@Null
+@NotBlank
+@ReportAsSingleViolation
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+public @interface NullOrNotBlank {
+
+    String message() default "{com.supervisor.utils.validation.NullOrNotBlank.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
